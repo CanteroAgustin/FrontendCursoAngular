7c2cd722ecc8cdf6786dbbdd9bb6291d5380ab4c
Carlos Agustin Cantero Rodriguez <agustin.cantero.so@softtekosde.com>
2020-07-12 21:32:49 -0300

    avance del curso

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HardcodedAuthenticationService } from '../services/hardcoded-authentication.service';
+import { BasicAuthenticationService } from '../services/basic-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,17 +14,34 @@ export class LoginComponent implements OnInit {
   password = '';
   errorMessage = 'Invalid Credentials!';
   invalidLogin = false;
-  constructor(private router: Router, private auth: HardcodedAuthenticationService) { }
+  constructor(
+    private router: Router,
+    private auth: HardcodedAuthenticationService,
+    private basicAuthenticationService: BasicAuthenticationService) { }
 
   ngOnInit() {
   }
 
   handleLogin() {
     if (this.auth.authenticate(this.userName, this.password)) {
-      this.router.navigate(['welcome',this.userName]);
+      this.router.navigate(['welcome', this.userName]);
       this.invalidLogin = false;
     } else {
       this.invalidLogin = true;
     }
+  }
+
+  handleBasicAuthLogin() {
+    this.basicAuthenticationService.executeBasicAuthenticationService(this.userName, this.password)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.router.navigate(['welcome', this.userName]);
+          this.invalidLogin = false;
+        },
+        error => {
+          console.log(error);
+          this.invalidLogin = true;
+        })
   }
 }
--- src/app/services/data/todo-data.service.ts
+++ src/app/services/data/todo-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from 'src/app/list-todos/list-todos.component';
+import { API_URL } from '../../app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +11,23 @@ export class TodoDataService {
   constructor(private http: HttpClient) { }
 
   public retrieveAllTodos(userName){
-    return this.http.get<Todo[]>(`http://localhost:8080/users/${userName}/todos`);
+    return this.http.get<Todo[]>(`${API_URL}/users/${userName}/todos`);
   }
 
   public deleteTodo(userName, id){
-    return this.http.delete(`http://localhost:8080/users/${userName}/todos/${id}`);
+    return this.http.delete(`${API_URL}/users/${userName}/todos/${id}`);
   }
 
   public retrieveTodo(userName, id){
-    return this.http.get<Todo>(`http://localhost:8080/users/${userName}/todos/${id}`);
+    return this.http.get<Todo>(`${API_URL}/users/${userName}/todos/${id}`);
   }
 
   public updateTodo(userName, id, todo){
-    return this.http.put(`http://localhost:8080/users/${userName}/todos/${id}`, todo);
+    return this.http.put(`${API_URL}/users/${userName}/todos/${id}`, todo);
   }
 
   public createTodo(userName, todo){
-    return this.http.post(`http://localhost:8080/users/${userName}/todos`, todo);
+    return this.http.post(`${API_URL}/users/${userName}/todos`, todo);
   }
-  
+
 }
